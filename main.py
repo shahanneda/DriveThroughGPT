b4f3c4b0242adcd11c5883f6da295687b500731e
Shahan Neda <shahan.neda@gmail.com>
2023-11-18 12:57:52 -0500

    merged vision with elevenlabs

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import time
 from openai import OpenAI
 import base64
+import os
 from dotenv import load_dotenv
+import elevenlabs as elabs
 
 load_dotenv()  
 
+eleven_labs_api_key = os.environ.get('ELEVEN_LABS_API_KEY')
+# Setup eleven labs
+assert (eleven_labs_api_key)
+elabs.set_api_key(eleven_labs_api_key)
+response = elabs.voices()
+obama = None
+for v in response.voices:
+		if v.name == "Obama Better":
+				obama = v
+
+assert (obama)
+# print(obama)
 
 
 client = OpenAI()
@@ -34,35 +48,46 @@ delay = 5
 # )
 
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+	with open(image_path, "rb") as image_file:
+		return base64.b64encode(image_file.read()).decode('utf-8')
 
 def ask_gpt_img():
-  base64_image = encode_image(image_path)
-  client = OpenAI()
-  response = client.chat.completions.create(
-    model="gpt-4-vision-preview",
-    messages=[
-      {
-        "role": "user",
-        "content": [
-          {"type": "text", "text": "What’s in this image?"},
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": f"data:image/jpeg;base64,{base64_image}"
-            },
-          },
-        ],
-      }
-    ],
-    max_tokens=300,
-  )
-  print(response.choices[0])
+	base64_image = encode_image(image_path)
+	client = OpenAI()
+	response = client.chat.completions.create(
+		model="gpt-4-vision-preview",
+		messages=[
+			{
+				"role": "user",
+				"content": [
+					{"type": "text", "text": "What’s in this image? Answer in a way obama would answer, in one sentance. Do not use let me clear at all times."},
+					{
+						"type": "image_url",
+						"image_url": {
+							"url": f"data:image/jpeg;base64,{base64_image}"
+						},
+					},
+				],
+			}
+		],
+		max_tokens=300,
+	)
+	res = response.choices[0].message.content
+	return res
+
+
+
+def say_eleven_labs(text):
+	audio = elabs.generate(voice=obama, text=text)
+	elabs.play(audio)
+
+# say_eleven_labs()
 
 while True:
 		print("Asking GPT!")
-		ask_gpt_img()
+		text = ask_gpt_img()
+		print("got ", text)
+		say_eleven_labs(text)
 		time.sleep(delay)
 
 
