bd064a0bca4d2f25b04e786baa957f56ad3a5f9c
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-11-18 12:14:26 -0500

    working audio output

--- 11labs.py
+++ 11labs.py
@@ -1,7 +1,6 @@
 import os
 
 import elevenlabs as elabs
-import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,8 +11,15 @@ elabs.set_api_key(eleven_labs_api_key)
 
 response = elabs.voices()
 
+obama = None
+
 for v in response.voices:
     if v.name == "Obama Better":
         obama = v
 
+assert (obama)
+
 print(obama)
+
+audio = elabs.generate(voice=obama, text="The basic API has a limited number of characters. To increase this limit, you can get a free API key from Elevenlabs (step-by-step guide) and set is as environment variable ELEVEN_API_KEY. Alternatively, you can provide the api_key string argument to the generate function, or set it globally in code with:")
+elabs.play(audio)
