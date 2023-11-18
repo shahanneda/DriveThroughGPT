0c19422c154db3f58678f5c9ef15a186425f4311
Shahan Neda <shahan.neda@gmail.com>
2023-11-18 12:34:33 -0500

    added gpt vision api

--- photo-taker.py
+++ photo-taker.py
@@ -1,37 +1,19 @@
+import cv2
 import time
+import os
 from openai import OpenAI
-import base64
 from dotenv import load_dotenv
+import base64
+
 
 load_dotenv()  
 
 
-
-client = OpenAI()
 folder = "images"
 image_path = f"{folder}/image.png"
+
 cam_port = 0
 delay = 5
-
-
-# response = client.chat.completions.create(
-#   model="gpt-4-vision-preview",
-#   messages=[
-#     {
-#       "role": "user",
-#       "content": [
-#         {"type": "text", "text": "What’s in this image?"},
-#         {
-#           "type": "image_url",
-#           "image_url": {
-#             "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-#           },
-#         },
-#       ],
-#     }
-#   ],
-#   max_tokens=300,
-# )
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
@@ -59,11 +41,37 @@ def ask_gpt_img():
     max_tokens=300,
   )
   print(response.choices[0])
+  
+cam = cv2.VideoCapture(cam_port) 
+i = 0
+fps = 24
+frame_count = 0
+save_interval = 1
+while cam.isOpened():
+    ret, frame = cam.read()
+    if ret:
+        frame_count += 1
 
-while True:
-		print("Asking GPT!")
-		ask_gpt_img()
-		time.sleep(delay)
+        if frame_count % (fps * save_interval) == 0:
+            print("saving frame!!")
+            cv2.imwrite(image_path, frame)
+            frame_count = 0
+    else:
+        break
+  
+# while True:
+# 		print("taking photo!")
+# 		result, image = cam.read() 
+# 		if not result:
+# 				print("No image detected. Please! try again") 
+# 				time.sleep(delay)
+# 				continue
 
+# 		print("writing photo")
+# 		# os.remove(image_path)
+# 		cv2.imwrite(f"images/{i}.png", image)
+# 		i += 1
+# 		# print("asking gpt")
+# 		# ask_gpt_img()
 
-print(response.choices[0])
+# 		time.sleep(delay)
